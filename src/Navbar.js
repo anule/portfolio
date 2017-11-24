16d9ba6a6a47204a51181db7d44cd693a67f95f0
anule <anule.ndukwu@gmail.com>
2017-11-24 14:23:29 -0500

    add Navbar component and tests

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <header>
+        <div id="logo">
+          <h1>
+            <a href="index.html">Anule Ndukwu</a>
+          </h1>
+        </div>
+        <nav>
+          <ul>
+            <li className="button">About Me</li>
+            <li className="button">Projects</li>
+            <li className="button">Blog</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+
+export default Navbar;
---
+++ src/Navbar.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './Navbar';
+
+it('renders navigation bar', () => {
+  const header = document.createElement('header');
+  ReactDOM.render(<Navbar />, header);
+});
+
+it('renders three buttons', () => {
+  expect();
+});
