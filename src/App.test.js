da171334b9bb121f0eddaabf204b6f06f7aea970
anule <anule.ndukwu@gmail.com>
2017-11-24 14:26:26 -0500

    app.js renders navbar

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
+import Navbar from './Navbar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders the navigation bar component', () => {
+  const app = shallow(<App />);
+  expect(app.contains(<Navbar />)).toEqual(true);
 });
