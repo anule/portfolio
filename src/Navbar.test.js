8dbce5f0a7aaf138d2a5930ce1656dbc75955cad
anule <anule.ndukwu@gmail.com>
2017-11-24 14:52:49 -0500

    Navbar component renders the list buttons

--- src/Navbar.test.js
+++ src/Navbar.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import Navbar from './Navbar';
+const nav = shallow(<Navbar />);
 
 it('renders navigation bar', () => {
   const header = document.createElement('header');
   ReactDOM.render(<Navbar />, header);
 });
 
-it('renders three buttons', () => {
-  expect();
+it('renders three list items', () => {
+  expect(nav.find('li').length).toEqual(3);
+});
+
+it('a list item has a "button" class name', () => {
+  expect(nav.containsMatchingElement(<li className="button">About Me</li>)).toEqual(true);
 });
