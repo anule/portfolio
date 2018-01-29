4330b3e0b42f302412ceaeeedb2bcd7205de6ea5
anule <anule.ndukwu@gmail.com>
2018-01-28 21:15:28 -0500

    add contact icons

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Contact.css';
+import './Anule-Ndukwu-Resume.pdf';
+
+const resumeLink = 'https://anule.github.io/Anule-Ndukwu-Resume.pdf';
 
 const Contact = () => (
   <section id="contact">
-    {/*<!-- Use Medium API to seed with my 3 most recent posts -->*/}
-    {/*<!-- Include link to medium post -->*/}
+    <h1 className="title">contact</h1>
+
+    <div id="contact-icons">
+      <a href="https://www.linkedin.com/in/anule-ndukwu">
+        <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
+      </a>
+      <a href="https://github.com/anule">
+        <i className="fa fa-github fa-4x" aria-hidden="true" />
+      </a>
+      <a href="mailto:anule.ndukwu@gmail.com">
+        <i className="fa fa-envelope fa-4x" aria-hidden="true" />
+      </a>
+      <a href="https://anulendukwu.space">
+        <i className="fa fa-camera-retro fa-4x" aria-hidden="true" />
+      </a>
+    </div>
   </section>
 );
 
